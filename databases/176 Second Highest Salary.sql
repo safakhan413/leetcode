417428f8330bfa668826c3eba2780bc90b191313
safakhan413 <49034994+safakhan413@users.noreply.github.com>
2020-10-24 22:38:00 +0800

    Update 176 Second Highest Salary.sql

--- databases/176 Second Highest Salary.sql
+++ databases/176 Second Highest Salary.sql
@@ -1,6 +1,7 @@
+# Write your MySQL query statement below
 SELECT
     IFNULL(
-    (SELECT Salary 
+    (SELECT DISTINCT Salary 
     FROM Employee
     ORDER BY Salary ASC
     LIMIT 1 OFFSET 1), NULL) AS SecondHighestSalary 
