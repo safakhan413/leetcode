f3a1a31de9471c8c25655362afede5f22e61e6c2
Safa Nasir <49034994+safakhan413@users.noreply.github.com>
2021-08-29 01:22:39 -0400

    Update 152. Maximum Product Subarray.py

--- Algorithms/medium/152. Maximum Product Subarray.py
+++ Algorithms/medium/152. Maximum Product Subarray.py
@@ -1,23 +1,4 @@
 def maxProduct(nums):
-
-    # dp = []
-    # dp.append(nums[0])
-    # current_largest_product = nums[0]
-    # sign_dict = dict()
-    # for i in range(1,len(nums)):
-    #     print(dp, dp[i - 1], nums[i], max(nums[i] * dp[i-1], nums[i]), nums[i])
-    #     if dp[i-1] <0:
-    #         sign_dict[i] = '-'
-    #     else:
-    #         sign_dict = '+'
-    #     dp.append( max(nums[i] * dp[i-1], nums[i]))
-    #     print('after appending', dp)
-    #
-    #     if dp[i] > current_largest_product:
-    #         current_largest_product = dp[i]
-    #         print('current_largest_product', current_largest_product)
-    #
-    # return  current_largest_product
     positive, negative = nums[0], nums[0]
     result = nums[0]
     for num in nums[1:]:
