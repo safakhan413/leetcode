37804bbcfea4353075652073ded4b47810ae3f72
Safa Nasir <49034994+safakhan413@users.noreply.github.com>
2021-09-03 16:56:16 -0400

    Update 74. Search a 2D Matrix.py

--- Algorithms/medium/74. Search a 2D Matrix.py
+++ Algorithms/medium/74. Search a 2D Matrix.py
@@ -1,3 +1,19 @@
+import numpy as np
+def binary_search_recursive(array, element, start, end):
+    if start > end:
+        return -1
+
+    mid = (start + end) // 2
+    print(start,end,mid)
+    if element == array[mid]:
+        print('here i am')
+        return mid
+
+    if element < array[mid]:
+        return binary_search_recursive(array, element, start, mid-1)
+    else:
+        return binary_search_recursive(array, element, mid+1, end)
+
 def searchMatrix( matrix, target):
     '''
     Algo:
@@ -7,23 +23,36 @@ def searchMatrix( matrix, target):
     - length of list -1
 
     '''
-    ### sol1
-    # lastElemeIdx = len(matrix[0]) - 1
-    # for listNum, v in enumerate(matrix):
-    #     for j in matrix[listNum]:
-    #         lastElem = v[lastElemeIdx]
-    #         if target <= lastElem:
-    #             if target in v:
-    #                 return True
-    #             else:
-    #                 return False #( as defined by the constraints of the matrix. it is sorted so target cant be in this array )
-    #         else:
-    #             continue
-
-
-# matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]]
-# target = 3
-matrix = [[1]]
-target = 1
+    ### sol3,1 better than 60% submissions
+    lastElemeIdx = len(matrix[0]) - 1
+    for listNum, v in enumerate(matrix):
+            lastElem = v[lastElemeIdx]
+            if target <= lastElem:
+                search = binary_search_recursive(v,target,0,lastElemeIdx)
+                # print(search)
+                if search >= 0:
+                    return True
+                else:
+                    return False
+                #### following was solution of solution1
+                # print(search)
+                # if target in v:
+                #     return True
+                # else:
+                #     return False #( as defined by the constraints of the matrix. it is sorted so target cant be in this array )
+            else:
+                continue
+############## slow solution
+    # numpy_mat = np.array(matrix)
+    # solutions = np.argwhere(numpy_mat == target)
+    #
+    # if len(solutions) > 0:
+    #     return True
+    # else:
+    #     return False
+matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]]
+target = 3
+# matrix = [[1]]
+# target = 1
 output = searchMatrix(matrix, target)
 print(output)
