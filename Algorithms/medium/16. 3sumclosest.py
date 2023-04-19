1f6d0eab7019c9296f5faf8dcd9a4ae21f02f3de
Safa Nasir <safakhan413@gmail.com>
2023-04-19 13:54:07 -0400

    Update 16. 3sumclosest.py

--- Algorithms/medium/16. 3sumclosest.py
+++ Algorithms/medium/16. 3sumclosest.py
@@ -1,11 +1,11 @@
 from itertools import combinations
-
+import math
 
 def threeSumClosest(nums, target):
     '''
     Algo:
     - inputs are nums and target. Find 3 ints in nums 
-    - output:the sum that is closest to target
+    - output:return the sum that is closest to target
     Steps:
         1st attempt:
         step1: make a set of 3 from list. Use combinations from itertools
@@ -17,7 +17,38 @@ def threeSumClosest(nums, target):
 
 
     '''
+    # print("hi i'm here")
+    # print(nums)
+    minDiff = math.inf
     comb = combinations(nums,3)
-    for i in 
-nums = [-1,2,1,-4],
+    minSum = -math.inf
+    # print(list(comb))
+    for i in list(comb):
+        print("why not here")
+        newSum = sum(i)
+        print(i,newSum)
+        newdiff = abs(target-newSum)
+        print('im new diff:', newdiff)
+        if newdiff < minDiff:
+            minDiff = newdiff
+            minSum = newSum
+    print('min sum is: ',minSum )
+    return minSum
+
+
+nums = [-1,2,1,-4]
 target = 1
+
+
+threeSumClosest(nums, target)
+
+# minDiff = math.inf
+#         comb = combinations(nums,3)
+#         minSum = -math.inf
+#         for i in list(comb):
+#             newSum = sum(i)
+#             newdiff = abs(target-newSum)
+#             if newdiff < minDiff:
+#                 minDiff = newdiff
+#                 minSum = newSum
+#         return minSum
