27aa272511a67bb8374e0c0121484b5eea3801ed
Safa Nasir <49034994+safakhan413@users.noreply.github.com>
2024-06-07 16:57:39 -0400

    2 more codes

---
+++ Algorithms/Easy/28. Find the Index of the First Occurrence in a String.py
@@ -0,0 +1,29 @@
+"""
+Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, 
+or -1 if needle is not part of haystack.
+
+ 
+
+Example 1:
+
+Input: haystack = "sadbutsad", needle = "sad"
+Output: 0
+Explanation: "sad" occurs at index 0 and 6.
+The first occurrence is at index 0, so we return 0.
+Example 2:
+
+Input: haystack = "leetcode", needle = "leeto"
+Output: -1
+Explanation: "leeto" did not occur in "leetcode", so we return -1.
+"""
+
+def strStr(haystack, needle):
+    print(f'printing haystack:: {haystack}, needle: {needle}')
+    ind = haystack.find(needle)
+    return ind
+
+if __name__ == "__main__":
+    haystack = "sadbutsad"
+    needle = "sad"
+    x  = strStr(haystack, needle)
+    print(x)
