ed200962d3b6c811a4b380f4ba5f4e8d54582071
Safa Nasir <49034994+safakhan413@users.noreply.github.com>
2024-07-16 05:12:50 -0400

    Update 300. Longest Increasing Subsequence.py

--- Algorithms/medium/300. Longest Increasing Subsequence.py
+++ Algorithms/medium/300. Longest Increasing Subsequence.py
@@ -31,56 +31,56 @@ Follow up: Can you come up with an algorithm that runs in O(n log(n)) time compl
 """
 
 def lengthOfLIS(nums):
-    """
-    Output:Given a list of nums give max LENGTH (longest) of increasing subsequnece
-    Lets do bruteforce first:
-    get all possible increasing subsequences in an array as tuples
-    find the max length of the tuple
-    input: Array of nums
+    # """
+    # Output:Given a list of nums give max LENGTH (longest) of increasing subsequnece
+    # Lets do bruteforce first:
+    # get all possible increasing subsequences in an array as tuples
+    # find the max length of the tuple
+    # input: Array of nums
 
-    Steps:
-    For each elem in nums, 
-    find max len subsequence
-    check an inreasing subsequence and add it to another list
+    # Steps:
+    # For each elem in nums, 
+    # find max len subsequence
+    # check an inreasing subsequence and add it to another list
 
     
-    """
-    print(nums)
-    if len(nums) == 1:
-        return 1
-    lis = []
-    for i, num in enumerate(nums):
-        lise = []
-        lise.append(nums[i])
-        print( 'im next i and num[i] \n  \n',i, nums[i])
-        for j in range(i, len(nums)):
-            print(nums[i], nums[j])
-            # if lise[-1] > nums[j] and nums[j] > nums[i]: # i.e. the last added j or last element is bigger than the next jth element then remove it and put smaller elemnt instead
-            #     print('i came here')
+    # """
+    # print(nums)
+    # if len(nums) == 1:
+    #     return 1
+    # lis = []
+    # for i, num in enumerate(nums):
+    #     lise = []
+    #     lise.append(nums[i])
+    #     print( 'im next i and num[i] \n  \n',i, nums[i])
+    #     for j in range(i, len(nums)):
+    #         print(nums[i], nums[j])
+    #         # if lise[-1] > nums[j] and nums[j] > nums[i]: # i.e. the last added j or last element is bigger than the next jth element then remove it and put smaller elemnt instead
+    #         #     print('i came here')
 
-            #     lise.remove(lise[-1])
-            #     lise.append(nums[j])
-            lastind = -1
-            while lise[lastind] > nums[j] and nums[j] > nums[i]:
-                print('im lise-1',lise[lastind], nums[j], lise[lastind] > nums[j], nums[j] > nums[i], lise)
+    #         #     lise.remove(lise[-1])
+    #         #     lise.append(nums[j])
+    #         lastind = -1
+    #         while lise[lastind] > nums[j] and nums[j] > nums[i]:
+    #             print('im lise-1',lise[lastind], nums[j], lise[lastind] > nums[j], nums[j] > nums[i], lise)
 
-                lise.remove(lise[lastind])
+    #             lise.remove(lise[lastind])
 
-                lastind -=1
-                lise.append(nums[j])
-                print('im removed', lise)
-                continue
-            if nums[j]> lise[-1]:
-                print('im when j is bigger than i', nums[j], nums[i])
+    #             lastind -=1
+    #             lise.append(nums[j])
+    #             print('im removed', lise)
+    #             continue
+    #         if nums[j]> lise[-1]:
+    #             print('im when j is bigger than i', nums[j], nums[i])
 
-                lise.append(nums[j])
-                print("new lise", lise)
-            # elif 
-        lis.append(lise)
-        print(lis, 'final list')
-        maxLen1 = len(max(lis, key= lambda x: len(x)))
-        print(f'max len {maxLen1}')
-    return maxLen1
+    #             lise.append(nums[j])
+    #             print("new lise", lise)
+    #         # elif 
+    #     lis.append(lise)
+    #     print(lis, 'final list')
+    #     maxLen1 = len(max(lis, key= lambda x: len(x)))
+    #     print(f'max len {maxLen1}')
+    # return maxLen1
         # return maxLen
         # return max(lis, key= lambda x: len(x))
 
@@ -108,6 +108,22 @@ def lengthOfLIS(nums):
     #     for j in range(i+1, len(nums)):
     #         # pass
     #         # check if 
+    if not nums:
+        return 0
+
+    # Create dp array with all elements initialized to 1
+    dp = [1] * len(nums)
+    max_len = 1  # At least one number in the sequence
+
+    # Loop through each number in the array
+    for i in range(len(nums)):
+        # Compare with all previous numbers
+        for j in range(i):
+            if nums[j] < nums[i]:
+                dp[i] = max(dp[i], dp[j] + 1)
+                max_len = max(max_len, dp[i])
+                
+    return max_len
         
 nums = [10,9,2,5,3,7,101,18]
 # nums = [0,1,0,3,2,3]
