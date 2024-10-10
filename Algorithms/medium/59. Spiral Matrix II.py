df07aba917471ca7d88c548a8a6d2f72921a64b3
Safa Nasir <49034994+safakhan413@users.noreply.github.com>
2024-10-09 23:54:21 -0400

    cleaning it up

--- Algorithms/medium/59. Spiral Matrix II.py
+++ Algorithms/medium/59. Spiral Matrix II.py
@@ -18,11 +18,7 @@
 # 1 <= n <= 20
 
 def generateMatrix(n):
-    import numpy as np
-    # np.array
-    # pass
     lst = [[None] * n for _ in range(n)]
-    print(lst)
 
     left, right=0,n
     top, bottom = 0, n
@@ -34,36 +30,28 @@ def generateMatrix(n):
         # Go through first row and fill it in spiral fashion
         for i in range(left, right):
             lst[left][i] = counter
-            print(top,i)
             counter +=1
         top +=1
 
         # Go through the last column and fill it in spiral fashion
         for i in range(top, bottom):
             lst[i][right-1]= counter
-            print('r', right-1,i)
             counter +=1
         right -=1
         # Go through the last row and fill it in spiral fashion
         for i in range(right-1, left-1,-1 ):
             lst[bottom-1][i]= counter
-            print('b', bottom-1,i)
             counter +=1
         bottom -=1
 
         # Go through the first col and fill it in spiral fashion
         for i in range(bottom-1, top-1,-1 ):
             lst[i][left]= counter
-            print('l', left,i)
             counter +=1
         left +=1
 
-    # top +=1
-    print(np.array(lst))
 
-
-    print(lst)
-    return list(lst)
+    return lst
 
 
     # arr = [None][None]*n
